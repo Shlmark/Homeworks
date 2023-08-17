77f3dd751f38e5152925f309f3d2758caaf30c6d
mark shlapak <shlapakmark2@gmail.com>
2023-08-17 19:47:18 +0300

    MyQueue added

---
+++ MyQueue1.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+
+class MyQueue
+{
+private:
+    class Node
+    {
+    public:
+        int data;
+        Node* pnext;
+        Node(int data)
+        {
+            this->data = data;
+        }
+    };
+    int size;
+    Node* head;
+    Node* tail;
+public:
+    MyQueue()
+    {
+        size = 0;
+        head = nullptr;
+        tail = nullptr;
+    }
+    bool empty()
+    {
+        if (head == nullptr)
+            return true;
+        return false;
+    }
+    int Size()
+    {
+        return this->size;
+    }
+    int front()
+    {
+        return head->data;
+    }
+    int back()
+    {
+        return tail->data;
+    }
+    void push(int val)
+    {
+        if (size == 0)
+        {
+            tail = new Node(val);
+            head = tail;
+            size++;
+        }
+        else
+        {
+            tail->pnext = new Node(val);
+            tail = tail->pnext;
+            size++;
+        }
+    }
+
+    void pop()
+    {
+        Node* temp = head->pnext;
+        delete head;
+        head = temp;
+        size--;
+    }
+    void Print()
+    {
+        Node* temp = this->head;
+        while (temp != nullptr)
+        {
+            std::cout << temp->data << ' ';
+            temp = temp->pnext;
+        }
+    }
+    ~MyQueue()
+    {
+        while (size)
+            this->pop();
+    }
+};
+
+int main()
+{
+    MyQueue a;
+    std::cout << a.empty() << '\n';
+    a.push(5);
+    a.push(2);
+    a.push(3);
+    a.push(7);
+    a.push(2);
+    a.pop();
+    a.Print();
+}
